b45eeb2d1318770284182c87b0514743cb6d35d4
Roy Kid <lijichen365@gmail.com>
2023-06-19 14:00:42 +0800

    error in CMakeLists

---
+++ src/packmol.py
@@ -0,0 +1,19 @@
+# author: Roy Kid
+# contact: lijichen365@126.com
+# date: 2023-06-19
+# version: 0.0.1
+
+class Packmol:
+
+    def __init__(self):
+        
+        # Printing title
+
+        pass
+
+    def pack(self):
+        
+        # Printing title
+
+        pass
+
